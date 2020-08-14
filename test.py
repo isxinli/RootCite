14417859c721d6b912e48f3379c1c8e0d0f0caa1
Xin Li <54775826+isxinli@users.noreply.github.com>
2020-08-14 12:56:31 -0500

    updating
    updating

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-thisPath = os.getcwd()
-path = thisPath.replace("\\", "\\\\") + "\\\\RootCiteProject" + "\\\\data_wos"
-print(path)
-
-for files in os.walk(path):
-    for filename in files[2]:
-        print(filename)
